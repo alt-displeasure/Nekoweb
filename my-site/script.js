5af27d085724cc982a768ab6b251a03c71d96906
Sofía <you@example.com>
2025-12-22 22:27:31 -0300

    Worked on the About me page and got the page Switcher to work

--- my-site/script.js
+++ my-site/script.js
@@ -10,3 +10,24 @@ function aboutSwitch() {
   document.getElementById("about").style.width = "55%";
   $("#about").load("about.html");
 }
+
+function homeSwitch() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("books").style.display = "none";
+  document.getElementById("blog").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("resources").style.display = "none";
+}
+
+function booksSwitch() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("books").style.display = "block";
+  document.getElementById("blog").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("resources").style.display = "none";
+
+  document.getElementById("books").style.width = "55%";
+  $("#books").load("books.html");
+}
